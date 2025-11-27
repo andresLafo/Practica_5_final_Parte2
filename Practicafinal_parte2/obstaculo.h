79b62b23972e0d66f04cfec8dda82c97074d3b41
andresLafo <andreslafaurie777@gmail.com>
2025-11-27 15:28:37 -0500

    Simulador de particulas

--- Practicafinal_parte2/obstaculo.h
+++ Practicafinal_parte2/obstaculo.h
@@ -1,10 +1,17 @@
 #ifndef OBSTACULO_H
 #define OBSTACULO_H
 
-class Obstaculo
-{
+#include "Particula.h"
+
+class Obstaculo {
 public:
-    Obstaculo();
+    double x, y, lado;
+    double resistencia;
+
+    Obstaculo(double x_=0, double y_=0, double lado_=10, double resistencia_=100);
+
+    bool colisiona(const Particula& p) const;
+    bool estaVivo() const { return lado > 0 && resistencia > 0; }
 };
 
-#endif // OBSTACULO_H
+#endif
